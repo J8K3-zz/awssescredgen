d358d65a379ab2060f17bc44886fff296d3f58fa
J8K3 <jacob.marks@gmail.com>
2018-01-04 20:24:58 -0500

    Migrated from CodePlex

---
+++ keygen.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace AWS.SMTP.CredentialGenerator
+{
+    partial class keygen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(keygen));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSecretKey = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.lblClipboardCountdown = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the AWS secret key associated with your IAM user for which you want to gene" +
+    "rate the SMTP password.";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(314, 73);
+            this.btnGenerate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(68, 19);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 53);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Secret Key:";
+            // 
+            // txtSecretKey
+            // 
+            this.txtSecretKey.Location = new System.Drawing.Point(74, 50);
+            this.txtSecretKey.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSecretKey.Name = "txtSecretKey";
+            this.txtSecretKey.Size = new System.Drawing.Size(308, 20);
+            this.txtSecretKey.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(74, 110);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.ReadOnly = true;
+            this.txtPassword.Size = new System.Drawing.Size(269, 20);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 112);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password:";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Image = ((System.Drawing.Image)(resources.GetObject("btnCopy.Image")));
+            this.btnCopy.Location = new System.Drawing.Point(347, 109);
+            this.btnCopy.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(18, 20);
+            this.btnCopy.TabIndex = 6;
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // lblClipboardCountdown
+            // 
+            this.lblClipboardCountdown.AutoSize = true;
+            this.lblClipboardCountdown.Location = new System.Drawing.Point(371, 112);
+            this.lblClipboardCountdown.Name = "lblClipboardCountdown";
+            this.lblClipboardCountdown.Size = new System.Drawing.Size(0, 13);
+            this.lblClipboardCountdown.TabIndex = 7;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(212, 153);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(170, 13);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://awscredgen.codeplex.com";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // keygen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 175);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.lblClipboardCountdown);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtSecretKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "keygen";
+            this.Text = "AWS SMTP Password Generator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.keygen_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSecretKey;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Label lblClipboardCountdown;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ keygen.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace AWS.SMTP.CredentialGenerator
+{
+    public partial class keygen : Form
+    {
+        //http://docs.aws.amazon.com/ses/latest/DeveloperGuide/smtp-credentials.html#smtp-credentials-convert
+
+        private static byte VERSION = 0x02; // Version number. Do not modify.
+        private static byte[] MESSAGE; // Used to generate the HMAC signature. Do not modify.
+
+        private Timer m_ClipboardTimer;
+
+        public keygen()
+        {
+            InitializeComponent();
+            this.m_ClipboardTimer = new Timer()
+            {
+                Interval = 1000
+            };
+            this.m_ClipboardTimer.Tick += ClipboardTimer_Tick;
+            this.linkLabel1.Links.Add(0, 32, "https://awscredgen.codeplex.com");
+        }
+
+        void ClipboardTimer_Tick(object sender, EventArgs e)
+        {
+            int count = Convert.ToInt32(this.lblClipboardCountdown.Text);
+
+            if (count == 1)
+            {
+                this.lblClipboardCountdown.Text = string.Empty;
+                this.m_ClipboardTimer.Stop();
+                Clipboard.Clear();
+            }
+            else
+            {
+                this.lblClipboardCountdown.Text = (count - 1).ToString();
+            }
+        }
+
+        static keygen()
+        {
+            keygen.MESSAGE = System.Text.Encoding.ASCII.GetBytes("SendRawEmail");
+        }
+
+        public static string GenerateCredentials(string awsSecretKey)
+        {
+            byte[] bytes = System.Text.Encoding.ASCII.GetBytes(awsSecretKey);
+
+            // Create an HMAC-SHA256 key from the raw bytes of the AWS secret access key.
+            HMACSHA256 secretKey = new HMACSHA256(bytes);
+
+            byte[] rawSignature = secretKey.ComputeHash(keygen.MESSAGE);
+
+            // Prepend the version number to the signature.
+            byte[] rawSignatureWithVersion = new byte[rawSignature.Length + 1];
+
+            byte[] versionArray = { VERSION };
+            Array.Copy(versionArray, 0, rawSignatureWithVersion, 0, 1);
+            Array.Copy(rawSignature, 0, rawSignatureWithVersion, 1, rawSignature.Length);
+            return Convert.ToBase64String(rawSignatureWithVersion);
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            this.txtPassword.Text = keygen.GenerateCredentials(this.txtSecretKey.Text);
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(this.txtPassword.Text);
+            this.m_ClipboardTimer.Start();
+            this.lblClipboardCountdown.Text = "30";
+        }
+
+        private void keygen_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Clipboard.Clear();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Link.LinkData.ToString());
+        }
+    }
+}
